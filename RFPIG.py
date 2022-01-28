834dc977d829eebc7c9a99612747c081f67fffab
happah99 <khaledhaddadi@hotmail.co.uk>
2022-01-28 17:36:52 +0000

    Created a home page that includes an initial input and imported "os" to help keep console and code clean.

--- RFPIG.py
+++ RFPIG.py
@@ -1,9 +1,24 @@
+import os
+
 fastF = ["mcD","kfc","pepes", "gdk"]
 
 midF = ["franzos", "nandos", "pho"]
 
 fancyF = ["saharaG", "gokyuzu"]
 
+def home():
+    os.system("cls")
 
-#x = fancyF[0]
-#print(x)
+    print("""
+     ___         ___        ___         ___          ___ 
+    | _ \       | __|      | _ \       |_ _|        / __|
+    |   /  _    | _|       |  _/  _     | |   _    | (_ |
+    |_|_\ (_)   |_|        |_|   (_)   |___| (_)    \___|
+    © Created by ShmuelIndustries
+
+    Welcome to the Random Food Place Idea Generator.
+    """)
+
+    input("Hopefully that name isn't the only mouthful you'll be experiencing today.")
+
+home()
