94250fca1e95ed5b2fcd32dd10ba9f1eed22eba6
happah99 <khaledhaddadi@hotmail.co.uk>
2022-01-28 22:37:45 +0000

    Turned the home page into a function so it can be called if needed, added a deserts array.

--- RFPIG.py
+++ RFPIG.py
@@ -6,19 +6,22 @@ midF = ["franzos", "nandos", "pho"]
 
 fancyF = ["saharaG", "gokyuzu"]
 
+desert = ["creams", "amorino"]
+
 def home():
     os.system("cls")
 
     print("""
-     ___         ___        ___         ___          ___ 
-    | _ \       | __|      | _ \       |_ _|        / __|
-    |   /  _    | _|       |  _/  _     | |   _    | (_ |
-    |_|_\ (_)   |_|        |_|   (_)   |___| (_)    \___|
-    © Created by ShmuelIndustries
+ ___         ___        ___         ___          ___ 
+| _ \       | __|      | _ \       |_ _|        / __|
+|   /  _    | _|       |  _/  _     | |   _    | (_ |
+|_|_\ (_)   |_|        |_|   (_)   |___| (_)    \___|
+© Created by ShmuelIndustries
 
-    Welcome to the Random Food Place Idea Generator.
+Welcome to the Random Food Place Idea Generator,
+Hopefully that name isn't the only mouthful you'll be experiencing today.
     """)
 
-    input("Hopefully that name isn't the only mouthful you'll be experiencing today.")
+    input("Please press any button to continue...")
 
 home()
