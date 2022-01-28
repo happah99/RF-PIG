d38652decc26d7b9d6f53c1927c92d6a34cdf8bc
happah99 <khaledhaddadi@hotmail.co.uk>
2022-01-28 23:38:40 +0000

    Added a second page with choices of which tier food is required. The choice is recorded as input and triggers an else if statement.

--- RFPIG.py
+++ RFPIG.py
@@ -1,12 +1,12 @@
 import os
 
-fastF = ["mcD","kfc","pepes", "gdk"]
+fastL = ["mcD","kfc","pepes", "gdk"]
 
-midF = ["franzos", "nandos", "pho"]
+midL = ["franzos", "nandos", "pho"]
 
-fancyF = ["saharaG", "gokyuzu"]
+fancyL = ["saharaG", "gokyuzu", "hakkasan"]
 
-desert = ["creams", "amorino"]
+dessertL = ["creams", "amorino", "bubbleT"]
 
 def home():
     os.system("cls")
@@ -22,6 +22,34 @@ Welcome to the Random Food Place Idea Generator,
 Hopefully that name isn't the only mouthful you'll be experiencing today.
     """)
 
-    input("Please press any button to continue...")
+    input("Please press ENTER to continue... ")
+    page1()
+
+def page1():
+    os.system("cls")
+
+    print("""
+
+The least you could do is choose what tier food you want:
+\nPress 1 for fast food
+\nPress 2 for mediocre food
+\nPress 3 for faaaancy food
+        """)
+    foodChoice = input("Please type in your choice here -> ")
+
+    if foodChoice == "1":
+        fastF()
+
+    elif foodChoice == "2":
+        midF()
+
+    elif foodChoice == "3":
+        fancyF()
+    
+    else:
+        os.system("cls")
+        print("\nYou must have misunderstood, excuse you,")
+        input("\nPlease press ENTER to continue... ")
+        page1()
 
 home()
