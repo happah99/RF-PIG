21e51216dcbfe8633e47068a5f3bbec44155764c
happah99 <khaledhaddadi@hotmail.co.uk>
2022-01-28 23:52:04 +0000

    Imported math and added dividers and sub dividers for print statements.

--- RFPIG.py
+++ RFPIG.py
@@ -1,4 +1,8 @@
 import os
+import math
+
+divider = "=" *80
+subDivider = "-" *80
 
 fastL = ["mcD","kfc","pepes", "gdk"]
 
@@ -17,25 +21,28 @@ def home():
 |   /  _    | _|       |  _/  _     | |   _    | (_ |
 |_|_\ (_)   |_|        |_|   (_)   |___| (_)    \___|
 © Created by ShmuelIndustries
-
-Welcome to the Random Food Place Idea Generator,
-Hopefully that name isn't the only mouthful you'll be experiencing today.
+    
     """)
+    
+    print(divider)
+    print("""Welcome to the Random Food Place Idea Generator,
+Hopefully that name isn't the only mouthful you'll be experiencing today.""")
+    print(divider)
 
-    input("Please press ENTER to continue... ")
+    input("\nPlease press ENTER to continue... ")
     page1()
 
 def page1():
     os.system("cls")
 
-    print("""
-
-The least you could do is choose what tier food you want:
+    print(subDivider)
+    print("""The least you could do is choose what tier food you want:
 \nPress 1 for fast food
 \nPress 2 for mediocre food
-\nPress 3 for faaaancy food
-        """)
-    foodChoice = input("Please type in your choice here -> ")
+\nPress 3 for faaaancy food""")
+    print(subDivider)
+
+    foodChoice = input("\nPlease type in your choice here -> ")
 
     if foodChoice == "1":
         fastF()
