26ba32deac6147591545f0051c83059bc3d20ed4
Zakariya B <zakariya1802@outlook.com>
2022-01-29 17:34:00 +0000

    Indented some stuff (#1)
    * Indented some stuff
    
    * Question function
    
    * Printing options
    
    * Clear work for unix
    
    * clear()

--- RFPIG.py
+++ RFPIG.py
@@ -1,19 +1,35 @@
 import os
 import math
 
-divider = "=" *80
-subDivider = "-" *80
+divider = "=" * 80
+subDivider = "-" * 80
 
-fastL = ["mcD","kfc","pepes", "gdk"]
-
+fastL = ["mcD", "kfc", "pepes", "gdk"]
 midL = ["franzos", "nandos", "pho"]
-
 fancyL = ["saharaG", "gokyuzu", "hakkasan"]
-
 dessertL = ["creams", "amorino", "bubbleT"]
 
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def interrogate(punchLine, questions):
+    clear()
+    choiceCounter = 0
+
+    print(divider)
+    print(punchLine)
+    print(divider)
+
+    for x in questions:
+        choiceCounter = choiceCounter + 1
+        print("Press " + str(choiceCounter) + " for " + x)
+
+    print(divider)
+    choice = input("Please type in your choice here -> ")
+    return choice 
+
 def home():
-    os.system("cls")
+    clear()
 
     print("""
  ___         ___        ___         ___          ___ 
@@ -26,35 +42,29 @@ def home():
     
     print(divider)
     print("""Welcome to the Random Food Place Idea Generator,
-Hopefully that name isn't the only mouthful you'll be experiencing today.""")
+    Hopefully that name isn't the only mouthful you'll be experiencing today.""")
     print(divider)
 
     input("\nPlease press ENTER to continue... ")
     page1()
 
 def page1():
-    os.system("cls")
-
-    print(subDivider)
-    print("""The least you could do is choose what tier food you want:
-\nPress 1 for fast food
-\nPress 2 for mediocre food
-\nPress 3 for faaaancy food""")
-    print(subDivider)
-
-    foodChoice = input("\nPlease type in your choice here -> ")
-
-    if foodChoice == "1":
-        fastF()
-
-    elif foodChoice == "2":
-        midF()
-
-    elif foodChoice == "3":
-        fancyF()
+    foodChoice = interrogate(
+        "The least you could do is choose what tier food you want:", 
+        ["fast food", "mediocre food", "faaaancy food"])
     
+    clear()
+    if foodChoice == "1":
+        for x in fastL:
+            print(x)
+    elif foodChoice == "2":
+       for x in midL:
+            print(x)
+    elif foodChoice == "3":
+        for x in midL:
+            print(x)
     else:
-        os.system("cls")
+        clear()
         print("\nYou must have misunderstood, excuse you,")
         input("\nPlease press ENTER to continue... ")
         page1()
