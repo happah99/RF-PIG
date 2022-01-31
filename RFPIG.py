37df2be9a567f5f8163a7cc9a4ddc8a3e25a450e
happah99 <khaledhaddadi@hotmail.co.uk>
2022-01-31 19:35:13 +0000

    Added a price range in foodPlaces, made two more pages to select price range and desert choice, added a randomiser to the final output

--- RFPIG.py
+++ RFPIG.py
@@ -1,6 +1,16 @@
 import os
 import math
 import json
+from re import X
+import textwrap
+from tkinter.ttk import Style
+import random
+
+global tierChoice
+global priceRangeChoice
+global isDessert
+
+wrapper = textwrap.TextWrapper(width=80)
 
 foodPlaces = json.load(open('foodPlaces.json'))
 divider = "=" * 80
@@ -8,6 +18,16 @@ subDivider = "-" * 80
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def niceOutput(x):
+    
+    desc = wrapper.fill(text=x["desc"])
+    print("\n" + divider)
+    print("Name: " + x["name"])
+    print(subDivider)
+    print("Tier: " + x["tier"])
+    print("Price range: " + x["priceRange"])
+    print("Description: " + desc + "\n")
 
 def interrogate(punchLine, questions):
     clear()
@@ -25,11 +45,11 @@ def interrogate(punchLine, questions):
     choice = input("Please type in your choice here -> ")
     return choice 
 
-def tierFilter(tier):
+def filterFoodPlaces(tier, priceRange, isDessert):
     filtered = []
     
     for x in foodPlaces:
-        if x["tier"] == tier:
+        if x["tier"] == tier and x["priceRange"] == priceRange and x["isDessert"] == isDessert:
             filtered.append(x)
     
     return filtered
@@ -48,31 +68,96 @@ def home():
     
     print(divider)
     print("""Welcome to the Random Food Place Idea Generator,
-    Hopefully that name isn't the only mouthful you'll be experiencing today.""")
+Hopefully that name isn't the only mouthful you'll be experiencing today.""")
     print(divider)
 
     input("\nPlease press ENTER to continue... ")
     page1()
 
 def page1():
+    
+    global tierChoice
+    global priceRangeChoice
+    global isDessert
+
     foodChoice = interrogate(
         "The least you could do is choose what tier food you want:", 
         ["fast food", "mediocre food", "faaaancy food"])
     
     clear()
     if foodChoice == "1":
-        for x in fastL:
-            print(x)
+        tierChoice = "Fast food"
+        page2()
     elif foodChoice == "2":
-       for x in midL:
-            print(x)
+        tierChoice = "Mediocre food"
+        page2()
     elif foodChoice == "3":
-        for x in midL:
-            print(x)
+        tierChoice = "Fancy food"
+        page2()
     else:
         clear()
         print("\nYou must have misunderstood, excuse you,")
         input("\nPlease press ENTER to continue... ")
         page1()
+
+def page2():
+    
+    global tierChoice
+    global priceRangeChoice
+    global isDessert
+
+    foodChoice = interrogate(
+        "How willing are you to split the bill?", 
+        ["Very willing", "Kinda...", "Im stingy"])
+    
+    clear()
+    if foodChoice == "1":
+        priceRangeChoice = "High"
+        page3()
+    elif foodChoice == "2":
+        priceRangeChoice = "Mid"
+        page3()
+    elif foodChoice == "3":
+        priceRangeChoice = "Low"
+        page3()
+    else:
+        clear()
+        print("\nYou must have misunderstood, excuse you,")
+        input("\nPlease press ENTER to continue... ")
+        page2()
+
+def page3():
+    
+    global tierChoice
+    global priceRangeChoice
+    global isDessert
+
+    foodChoice = interrogate(
+        "How willing are you to split the bill?", 
+        ["Sweet", "Salty"])
+    
+    clear()
+    if foodChoice == "1":
+        isDessert = True
+        page4()
+    elif foodChoice == "2":
+        isDessert = False
+        page4()
+
+    else:
+        clear()
+        print("\nYou must have misunderstood, excuse you,")
+        input("\nPlease press ENTER to continue... ")
+        page3()
+
+def page4():
+    finalChoice = filterFoodPlaces(tierChoice, priceRangeChoice, isDessert)
+    size = len(finalChoice)
+    randomPos = random.randint(0, size) - 1
+
+    
+    print("\nYour choice has been made: Bon Appetite!")
+
+    niceOutput(finalChoice[randomPos])
 
 home()
